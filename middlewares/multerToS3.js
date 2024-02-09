ad2f58aeeb05241815fc176f01b4aaf31131daf3
Manu-Madhu <manum66466@gmail.com>
2024-02-09 18:55:13 +0530

    Update the multer size

--- middlewares/multerToS3.js
+++ middlewares/multerToS3.js
@@ -1,6 +1,6 @@
 const multer = require('multer');
 const multerS3 = require('multer-s3');
-const {S3Client} = require('@aws-sdk/client-s3');
+const { S3Client } = require('@aws-sdk/client-s3');
 
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
@@ -9,26 +9,26 @@ const Bucket = process.env.S3_BUCKET;
 
 const s3Client = new S3Client({
     region: region,
-    credentials:{
-        accessKeyId:accessKeyId,
-        secretAccessKey:secretAccessKey
+    credentials: {
+        accessKeyId: accessKeyId,
+        secretAccessKey: secretAccessKey
     }
 })
 
 // Upload files to AWS S3 Bucket;
 
 const upload = multer({
-    
+    limits: { fileSize: 10 * 1024 * 1024 },
     storage: multerS3({
         s3: s3Client,
-        bucket:Bucket,
-        key:(req,file,cb)=>{
+        bucket: Bucket,
+        key: (req, file, cb) => {
             const uniquePrefix = Date.now() + '-' + Math.round(Math.random() * 1e9)
             cb(null, 'Documents/' + `${uniquePrefix}-${file.originalname}`);
         },
-        
+
     }),
-    
+
 });
 
 module.exports = upload;
