f67cd427867aba2eb0d73e99fe4fcd58feae1cf1
Manu-Madhu <manum66466@gmail.com>
2024-02-09 21:22:50 +0530

    Adjust  the size with help of bodyParser

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const authRouter = require("./routes/AuthRoutes")
 const applicationRouter = require("./routes/ApplicationRoutes");
 const cors = require('cors');
+const bodyParser = require('body-parser')
 
 const ClientURL = process.env.ClientURL;
 
@@ -17,6 +18,7 @@ app.use(cors({
 
 app.use(CookieParser())
 app.use(express.json())
+app.use(bodyParser.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ extended: true }))
 
 app.use("/api/auth", authRouter);
