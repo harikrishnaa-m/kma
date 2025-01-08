a005c5983b44ba1f4bccb967097c7bcedabd4260
abhijth.r.b <abhijithrb91@gmail.com>
2025-01-08 13:53:53 +0530

    file upload to DO done

--- middlewares/multerToS3.js
+++ middlewares/multerToS3.js
@@ -2,12 +2,14 @@ const multer = require('multer');
 const multerS3 = require('multer-s3');
 const { S3Client } = require('@aws-sdk/client-s3');
 
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
-const region = process.env.S3_REGION;
-const Bucket = process.env.S3_BUCKET;
+const accessKeyId = process.env.DO_SPACES_ACCESS_KEY_ID;
+const secretAccessKey = process.env.DO_SPACES_SECRET_ACCESS_KEY;
+const endpoint = process.env.DO_SPACES_ENDPOINT;
+const region = process.env.DO_SPACES_REGION;
+const Bucket = process.env.DO_SPACES_BUCKET;
 
 const s3Client = new S3Client({
+    endpoint: endpoint,
     region: region,
     credentials: {
         accessKeyId: accessKeyId,
@@ -15,18 +17,47 @@ const s3Client = new S3Client({
     }
 })
 
-// Upload files to AWS S3 Bucket;
+
 const upload = multer({
-    limits: { fileSize: 25 * 1024 * 1024 },
     storage: multerS3({
         s3: s3Client,
         bucket: Bucket,
         key: (req, file, cb) => {
-            const uniquePrefix = Date.now() + '-' + Math.round(Math.random() * 1e9)
-            cb(null, 'Documents/' + `${uniquePrefix}-${file.originalname}`);
+            const uniquePrefix = Date.now().toString() + '-' + Math.round(Math.random() * 1e9)
+            cb(null, 'Media/' + `${uniquePrefix}-${file.originalname}`);
+        },
+        acl: 'public-read',
+        contentDisposition: 'inline',
+
+        contentType: (req, file, cb) => {
+            let contentType = 'application/octet-stream';
+
+            switch (file.mimetype) {
+                case 'image/jpeg':
+                    contentType = 'image/jpeg';
+                    break;
+                case 'image/png':
+                    contentType = 'image/png';
+                    break;
+                case 'application/pdf':
+                    contentType = 'application/pdf';
+                    break;
+                case 'video/mp4':
+                    contentType = 'video/mp4';
+                    break;
+                case 'audio/mpeg':
+                    contentType = 'audio/mpeg';
+                    break;
+                default:
+                    contentType = 'application/octet-stream'; 
+                    break;
+            }
+
+            cb(null, contentType); 
         },
     }),
+
 });
 
 
-module.exports = upload;
+module.exports = { upload, s3Client , Bucket}
