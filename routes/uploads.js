69e60fccfb14af2e602b0de7a98339d6605e87f4
abhijth.r.b <abhijithrb91@gmail.com>
2025-01-09 15:14:25 +0530

    bug fix

--- routes/uploads.js
+++ routes/uploads.js
@@ -4,4 +4,4 @@ const uploadRouter = require("express").Router();
 uploadRouter.post('/single', commonCtrl.uploadSingleFile)
 uploadRouter.post('/multiple', commonCtrl.uploadMultipleFile)
 
-module.exports = { uploadRouter }
+module.exports =  uploadRouter 
